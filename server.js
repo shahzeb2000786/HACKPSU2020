c0cd8adf0f72eb39beb8175f238cd47b56c65641
Shahzeb Ahmed <shahzeb2000786@gmail.com>
2021-02-10 00:21:57 -0500

    Removed stock class from server file

--- server.js
+++ server.js
@@ -1,31 +1,4 @@
-//const Stock = require("./StockModel.js");
-class Stock {
-  constructor(ticker,open,high,low, price, volume, date, previousClose, change,
-    changePercent, exchange, sector, industry, fiftyTwoHigh, fiftyTwoLow,
-    peRatio, marketCap,dividendYield, fiftyDayMovingAverage, description){
-    this.ticker = ticker
-    this.open = open;
-    this.high = high;
-    this.low = low;
-    this.price = price
-    this.volume = volume;
-    this.date = date;
-    this.previousClose = previousClose;
-    this.change = change;
-    this.changePercent = changePercent;
-    this.exchange = exchange
-    this.sector = sector
-    this.industry = industry
-    this.fiftyTwoHigh = fiftyTwoHigh;
-    this.fiftyTwoLow = fiftyTwoLow;
-    this.peRatio = peRatio;
-    this.marketCap = marketCap;
-    this.dividendYield = dividendYield;
-    this.fiftyDayMovingAverage = fiftyDayMovingAverage
-    this.description = description;
-
-  }//end of constructor
-}
+const Stock = require("./StockModel.js");
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
